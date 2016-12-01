5c63e5567caa6b8acabe30c520979e7b3c721d08
ALittleNasty <379643671@qq.com>
2016-12-01 10:10:06 +0800

    初始化项目

---
+++ CollectionViewDemo/CollectionViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionViewDemo
+//
+//  Created by ALittleNasty on 2016/12/1.
+//  Copyright © 2016年 vcredit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
